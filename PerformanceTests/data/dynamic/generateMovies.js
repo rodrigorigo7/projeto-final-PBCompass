a76bf35993b8003bf45bb54adf8e14844616e184
RodrigoRigo <rodrigo.rigo@outlook.com.br>
2025-03-07 15:15:58 -0300

    adicionando os arquivos de dados estáticos (faker)

--- PerformanceTests/data/dynamic/generateMovies.js
+++ PerformanceTests/data/dynamic/generateMovies.js
@@ -1,1 +1,18 @@
 import faker from "https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js";
+
+const generateMovies = () => {
+    const movies = [];
+    for (let i = 0; i < 150; i++) {
+      movies.push({
+        title: faker.lorem.words(4),
+        description: faker.lorem.paragraph(),
+        launchdate: faker.date.past({ year: 3 }).toISOString(), // Formato ISO 8601 para data
+        showtimes: [
+            faker.date.between('2025-01-01', '2025-03-10'),
+            faker.date.between('2025-01-01', '2025-03-10'),
+            faker.date.between('2025-01-01', '2025-03-10')
+        ],
+      });
+    }
+    return { movies };
+  };
--- PerformanceTests/data/dynamic/generateTickets.js
+++ PerformanceTests/data/dynamic/generateTickets.js
@@ -1,1 +1,26 @@
 import faker from "https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js";
+
+const getMovies = async () => (await fetch('http://localhost:3000/movies')).json().movies;
+
+const generateTickets = async () => {
+  const movies = await getMovies();
+  const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I','J'];
+  const usedSeats = new Set();
+
+  return Array.from({ length: 150 }, () => {
+    const movie = faker.random.arrayElement(movies);
+    let seat;
+    do {
+      seat = `${faker.random.arrayElement(rows)}${faker.random.number({ min: 1, max: 10 })}`;
+    } while (usedSeats.has(seat));
+    usedSeats.add(seat);
+
+    return {
+      movieId: movie.id,
+      sessionId: faker.random.number({ min: 1, max: 150 }),
+      seat,
+      price: faker.random.number({ min: 1, max: 60 }),
+      status: "AVAILABLE"
+    };
+  });
+};
